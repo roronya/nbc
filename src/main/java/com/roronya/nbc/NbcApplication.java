f63ef332d0e7b58415d13752a03516d11f023c06
roronya <roronya628@gmail.com>
2020-06-27 19:44:27 +0900

    init

---
+++ src/main/java/com/roronya/nbc/NbcApplication.java
@@ -0,0 +1,13 @@
+package com.roronya.nbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbcApplication.class, args);
+	}
+
+}
